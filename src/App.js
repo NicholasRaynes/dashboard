842ddd6edd1fc3df0e5de0f0d2e2b8c739f04b66
NicholasRaynes <nicholasraynesmb@gmail.com>
2023-10-17 16:24:54 -0500

    Add links, icons, logo, and exit button to the sidebar

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ const App = () => {
             <div className="fixed md:static
             bg-main-bg dark:bg-main-dark-bg
             navbar w-full">
-              <Sidebar />
+              
             </div>
           </div>
           <div>
